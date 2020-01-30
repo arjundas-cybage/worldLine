ff5deb62a1021cc07a10479d2b709161c1ccaf96
Arjun Das <arjund@cybage.com>
2020-01-30 18:47:12 +0530

    Second Commit

--- Assignment1/Problem1/src/SundaysCalculator.js
+++ Assignment1/Problem1/src/SundaysCalculator.js
@@ -2,22 +2,24 @@
 
 const fs = require('fs');
 const message = " Sunday's fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000) .";
-fs.writeFile("../Output.txt", firstSundays(), function (err) {
+var fromDate  = 1901;
+var toDate    = 2001;
+fs.writeFile("../Output.txt", firstSundays(fromDate, toDate), function (err) {
   if (err) {
     return console.log(err);
   }
   console.log("The file was saved!");
 });
 
-function firstSundays() {
+function firstSundays(fromDate,toDate ) {
   //Declaration of week in an array 
   var daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   var countOfSundays = 0;
-  //iterating over the years
-  for (let year = 1901; year < 2001; year++) {
+    //iterating over the years
+  for (; fromDate < toDate; fromDate++) {
   //iterating over the months
     for (let month = 1; month < 13; month++) {
-      let specificDate = new Date(year, month, 1);
+      let specificDate = new Date(fromDate, month, 1);
       //finding sundays
       let day = daysOfWeek[specificDate.getDay()];
       if (day == 'Sun') {
